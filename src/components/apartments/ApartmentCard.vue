ad1b77b94c886ccf535594faaa77b0268faa822f
dieghino26 <brotzadiego@gmail.com>
2024-04-26 17:40:54 +0200

    added card

--- src/components/apartments/ApartmentCard.vue
+++ src/components/apartments/ApartmentCard.vue
@@ -6,6 +6,15 @@ export default {
 </script>
 
 <template>
+    <div class="card">
+        <img src="../../assets/homephoto.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5>{{ apartment.title }}</h5>
+            <p class="mb-0">{{ apartment.address }}</p>
+            <p class="mb-0">€{{ apartment.price_per_night }}</p>
+            <p class="mb-0">{{ apartment.square_meters }} m<sup>2</sup></p>
+        </div>
+    </div>
 </template>
 
-<style></style>
+<style scoped lang="scss"></style>
