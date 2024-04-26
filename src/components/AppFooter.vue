0e86939bbe447ad4ad24495087cf99a6e8baa3db
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-26 17:18:49 +0200

    created appfooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+<template>
+    <footer>
+        <div class="container-fluid">
+            <label class="mb-0 ms-5 text-secondary">
+                &copy; 2024 BoolBnB
+            </label>
+        </div>
+    </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+    height: 60px;
+    line-height: 60px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    transition: 0.5s;
+    background-color: rgb(255, 255, 255);
+    box-shadow: 0px -2px 2px -2px rgba(0, 0, 0, 0.5);
+}
+</style>
