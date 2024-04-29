6dcb6c9fe0ca2a4616f8c0c29c86ca87f5231d4d
dieghino26 <brotzadiego@gmail.com>
2024-04-29 13:08:25 +0200

    added axios call in detail page

--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -1,11 +1,36 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/apartments/';
 export default {
     name: 'DetailPage',
+    data: () => ({
+        apartment: null
+    }),
+    methods: {
+        async getApartment() {
+            try {
+                const res = await axios.get(endpoint + this.$route.params.slug)
+                this.apartment = res.data
+            } catch (err) {
+                console.error(err)
+                this.$router.push({ name: 'not-fount' })
+            }
+
+
+        }
+    },
+    created() {
+        this.getApartment();
+
+    }
 }
 </script>
 
 <template>
+    <div v-if="apartment">
+        <h4>{{ apartment.title }}</h4>
+    </div>
 
 </template>
 
-<style></style>
+<style scoped lang="scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../pages/HomePage.vue';
 import NotFoundPage from '../pages/NotFoundPage.vue';
+import DetailPage from '../pages/DetailPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", name: "home", component: HomePage },
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
+        { path: '/detail-page/:slug', component: DetailPage, name: 'detail-page' },
         { path: "/:pathMatch(.*)*", redirect: "/not-found" }
     ]
 });
