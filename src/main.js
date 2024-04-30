c9b49549353e5caad7788f8e52488486b08bf8dd
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-30 17:02:12 +0200

    added custom icon

--- src/main.js
+++ src/main.js
@@ -5,19 +5,19 @@ import AppLoader from './components/AppLoader.vue'
 import { router } from './router/index.js'
 
 /* import the fontawesome core */
-// import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-// import { all } from '@awesome.me/kit-KIT_CODE/icons'
+import { faSort } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-// library.add(...all)
+library.add(faSort)
 
 createApp(App)
-    // .component('font-awesome-icon', FontAwesomeIcon)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .component('AppLoader', AppLoader)
     .use(router)
     .mount('#app')
